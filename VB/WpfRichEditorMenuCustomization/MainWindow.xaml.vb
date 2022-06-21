75d9e07be069011879db670498be7557621be4dd
DevExpressExampleBot <fakeEmail>
2022-06-21 15:09:01 +0400

    autogenerate VB

--- VB/WpfRichEditorMenuCustomization/MainWindow.xaml.vb
+++ VB/WpfRichEditorMenuCustomization/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-Imports System
+Imports System.Linq
 
 Namespace WpfRichEditorMenuCustomization
 
@@ -9,16 +9,16 @@ Namespace WpfRichEditorMenuCustomization
         Inherits DevExpress.Xpf.Core.ThemedWindow
 
         Public Sub New()
-            Me.InitializeComponent()
-            Me.richTextEditor.Document.Text = "The WPF Rich Text Editor allows you to integrate word processing " & "capabilities into your next WPF project. With its comprehensive text formatting options, " & "mail-merge, and a rich collection of end-user options you can deliver Microsoft Word-inspired " & "functionality with ease."
+            InitializeComponent()
+            richTextEditor.Document.Text = "The WPF Rich Text Editor allows you to integrate word processing " & "capabilities into your next WPF project. With its comprehensive text formatting options, " & "mail-merge, and a rich collection of end-user options you can deliver Microsoft Word-inspired " & "functionality with ease."
         End Sub
 
         Private Sub HighlightSelection_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            Dim selectedRanges = Me.richTextEditor.Document.Selections
+            Dim selectedRanges = richTextEditor.Document.Selections
             For Each range In selectedRanges
-                Dim charProps = Me.richTextEditor.Document.BeginUpdateCharacters(range)
+                Dim charProps = richTextEditor.Document.BeginUpdateCharacters(range)
                 charProps.BackColor = System.Drawing.Color.Yellow
-                Me.richTextEditor.Document.EndUpdateCharacters(charProps)
+                richTextEditor.Document.EndUpdateCharacters(charProps)
             Next
         End Sub
     End Class
